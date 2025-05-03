6a8e493a7dcfd0869564f9637979d25646343cd1
github_ilhmfadl <ilhamfadli741@gmail.com>
2025-05-03 21:21:33 +0700

    User Dapat membatalkan kegiatan yang telah di inputkan

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,10 @@ function addTodo() {
   todos.value.push({ id: id++, text: newTodo.value })
   newTodo.value = ''
 }
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
 </script>
 
 <template>
@@ -35,6 +39,7 @@ function addTodo() {
   <ul>
     <li v-for="todo in todos" :key="todo.id">
       {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
 </template>
