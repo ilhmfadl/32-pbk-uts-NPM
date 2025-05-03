34f8a34039debd1ee8a56655bfa2258e4eba0d02
github_ilhmfadl <ilhamfadli741@gmail.com>
2025-05-03 21:13:21 +0700

    User Dapat melihat kegiatan yang sudah ditambahkan di aplikasi

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+// unique id untuk tiap todo
+let id = 0
+
+const newTodo = ref('')
+const todos = ref([
+  { id: id++, text: 'Sholat subuh' },
+  { id: id++, text: 'Olahraga ringan' },
+  { id: id++, text: 'Sarapan' },
+  { id: id++, text: 'Berangkat Kuliah' },
+  { id: id++, text: 'Istirahat siang' },
+  { id: id++, text: 'Sholat Zuhur' },
+  { id: id++, text: 'Pulang & bersih-bersih' },
+  { id: id++, text: 'Sholat Ashar' },
+  { id: id++, text: 'Jogging' },
+  { id: id++, text: 'Mandi' },
+  { id: id++, text: 'Sholat Maghrib' },
+  { id: id++, text: 'Makan malam' },
+  { id: id++, text: 'Sholat isya' }
+])
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+    </li>
+  </ul>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
